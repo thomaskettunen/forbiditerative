9f37254cad94c18b0586c4f9f4830126e1c24cdd
Xonthar <94834027+Xonthar@users.noreply.github.com>
2026-01-07 12:57:21 +0100

    Add Exit code table

--- forbiditer_lab/Analysis.py
+++ forbiditer_lab/Analysis.py
@@ -28,6 +28,25 @@ def ReadData(parameter):
                 parameterValues[runs[planner][task]["domain"]] = results
     return parameterValues
 
+def CountExitCodes():
+    planners = ["our_run_prefix1", "our_run_prefix2"]
+    results={}
+    for planner in planners:
+        results[planner] = {}
+    for task in tasks:
+        error = False
+        for planner in runs.keys():
+            if not "exit code" in runs[planner][task]:
+                error = True
+        if(not error):        
+            for planner in planners:
+                if runs[planner][task]["exit code"] in results[planner]:
+                    results[planner][runs[planner][task]["exit code"]] += 1
+                else:
+                    results[planner][runs[planner][task]["exit code"]] = 0
+    return results
+
+
 def GetTotals(results):
     total = {}
     for planner in runs.keys():
@@ -69,6 +88,26 @@ def generateTable(resultDict, table):
             f.write(line + "\n")
             f.write("\\hline \n")
 
+def generateExitCodeTable(exitCodeDict):
+    with open("analysisFolder/ExitCodes", "w") as f:
+        f.write("\\hline \n")
+        line = "Exit Code & "
+        for planner in exitCodeDict.keys():
+            line += f"{planner} & "
+        line = line.removesuffix(" & ")
+        line += " \\\\"
+        f.write(line + "\n")
+        f.write("\\hline \n")
+        for code in exitCodeDict["our_run_prefix1"]:
+            line = f"{code} & "
+            for planner in exitCodeDict.keys():
+                line += f"{exitCodeDict[planner][code]} & "
+            line = line.removesuffix(" & ")
+            line += " \\\\"
+            f.write(line + "\n")
+            f.write("\\hline \n")
+
+
 parameters = ["coverage", "plans found", "total time"]
 totals = {}
 for parameter in parameters:
@@ -78,3 +117,4 @@ for parameter in parameters:
     generateTable(dataDiff, "table_" + parameter.replace(" ", "_"))
 
 generateTable(totals, "table_totals")
+generateExitCodeTable(CountExitCodes())
