6427c5e34143a85ae59a1c80eb9f20e65964ce8c
MBStarup <mavi.nexu@gmail.com>
2026-01-25 14:55:38 +0100

    prefix(1)

--- forbiditer_lab/experiments.py
+++ forbiditer_lab/experiments.py
@@ -108,7 +108,7 @@ from parser import FIParser
 exp.add_parser(FIParser())
 
 SEARCH = "forbiditer"
-GROUPING = "none"
+GROUPING = "prefix(1)"
 
 for task in suites.build_suite(BENCHMARKS_DIR, SUITE):
     run = exp.add_run()
